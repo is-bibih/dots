661c0b6c0c6cae2fa24f9272b342bb84583e1812
vivi <vivianamaldes@gmail.com>
2020-01-11 19:28:59 -0600

    makes script stop notifications when on ac power

--- lowbat.sh
+++ lowbat.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+ACPATH=/sys/class/power_supply/ACAD
 BATPATH=/sys/class/power_supply/BAT1
+AC=$ACPATH/online
 BAT_FULL=$BATPATH/charge_full
 BAT_NOW=$BATPATH/charge_now
 bf=$(cat $BAT_FULL)
 bn=$(cat $BAT_NOW)
+ac=$(cat $AC)
 low=15
 lower=7
-if [ $(( 100 * $bn / $bf )) -lt $lower ]; then
+if [ "$ac" = "1" ]; then
+    :
+elif [ $(( 100 * $bn / $bf )) -lt $lower ]; then
     notify-send "battery is too low >:c"
 elif [ $(( 100 * $bn / $bf )) -lt $low ]; then
     notify-send "battery is low"
