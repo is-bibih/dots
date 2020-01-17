aaa8bddca8eaa26c67df1f56cef10c7cf08bb6f3
vivi <vivianamaldes@gmail.com>
2020-01-17 10:38:11 -0600

    makes the code prettier

--- lowbat.sh
+++ lowbat.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+# path to ac and battery info
 ACPATH=/sys/class/power_supply/ACAD
 BATPATH=/sys/class/power_supply/BAT1
-AC=$ACPATH/online
+
+# low battery percents to send notifications at
+LOW=15
+LOWER=7
+
+# battery full charge and actual charge
 BAT_FULL=$BATPATH/charge_full
 BAT_NOW=$BATPATH/charge_now
-bf=$(cat $BAT_FULL)
-bn=$(cat $BAT_NOW)
-ac=$(cat $AC)
-low=15
-lower=7
 
-if [ $(( 100 * $bn / $bf )) -eq 100 ]; then
+# actual battery percent
+bat=$(( 100 * $(cat $BAT_NOW) / $(cat $BAT_FULL) ))
+
+# is ac power supply connected
+ac=$(cat $ACPATH/online)
+
+
+if [ $bat -eq 100 ]; then
     notify-send "battery is full"
-elif [ "$ac" = "1" ]; then
+elif [ $ac -eq 1 ]; then
     :
-elif [ $(( 100 * $bn / $bf )) -lt $lower ]; then
+elif [ $bat -lt $LOWER ]; then
     notify-send -u critical "battery is too low >:c"
-elif [ $(( 100 * $bn / $bf )) -lt $low ]; then
+elif [ $bat -lt $LOW ]; then
     notify-send "battery is low"
 fi
