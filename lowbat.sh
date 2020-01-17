cc8fcd98201bdc5f11214e76c269e0a9474a4371
vivi <vivianamaldes@gmail.com>
2020-01-17 10:27:37 -0600

    defines urgency level and adds notification for 100%

--- lowbat.sh
+++ lowbat.sh
@@ -10,10 +10,13 @@ bn=$(cat $BAT_NOW)
 ac=$(cat $AC)
 low=15
 lower=7
-if [ "$ac" = "1" ]; then
+
+if [ $(( 100 * $bn / $bf )) -eq 100 ]; then
+    notify-send "battery is full"
+elif [ "$ac" = "1" ]; then
     :
 elif [ $(( 100 * $bn / $bf )) -lt $lower ]; then
-    notify-send "battery is too low >:c"
+    notify-send -u critical "battery is too low >:c"
 elif [ $(( 100 * $bn / $bf )) -lt $low ]; then
     notify-send "battery is low"
 fi
