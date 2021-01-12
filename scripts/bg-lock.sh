d58d2481dbcbbdb274cd518d1b967ee4af92f582
vivi <vivianamaldes@gmail.com>
2021-01-11 19:39:34 -0600

    uses betterlockscreen for screen locking

---
+++ scripts/bg-lock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BG_DIR=$HOME/.wallpapers/
+
+BG_FILE=$(find $BG_DIR -type f | shuf -n 1)
+
+# set bg
+feh --bg-fill --no-fehbg $BG_FILE
+
+# cache image for lockscreen
+betterlockscreen -u $BG_FILE -b 1
